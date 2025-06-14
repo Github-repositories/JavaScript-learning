e519511bac715b9cf92a5c1e157bec1e6b56189b
Github-repositories <166438039+Github-repositories@users.noreply.github.com>
2025-06-14 18:34:18 +0000

    Added file to find unique email Ids

---
+++ emailId.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const readEmailIds = () => {
+    const sourceFilePath = './emailId.txt';
+    const destFilePath = './uniqueEmailId.txt';
+    const destFile = './uniqueEmails.txt';
+    const readFile = fs.readFileSync(sourceFilePath, 'UTF-8');
+    const uniqueEmailId = [...new Set(readFile.split('\n').map(line => line.trim()))].filter(line => line);
+    fs.writeFileSync(destFilePath, uniqueEmailId.join('\n'), 'UTF-8');
+}
+readEmailIds();
